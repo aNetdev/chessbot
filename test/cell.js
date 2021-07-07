85ed2a5b91bf98c702b4c5b1d5f982fc4168de82
aNetDev <arunsreekanth@gmail.com>
2021-07-07 19:18:13 -0400

    refactoring

---
+++ test/cell.js
@@ -0,0 +1,32 @@
+class Cell {
+    constructor(x, y, len, color) {
+        this.x = x;
+        this.y = y;
+        this.len = len;
+        this.color = color;
+
+    }
+    midpoint() {
+
+        return { x: this.x + (this.len / 2), y: this.y + (this.len / 2) };
+    }
+    show() {
+        push();
+        noStroke();
+        fill(this.color);
+        rect(this.x, this.y, this.len, this.len);
+        
+        if (this.occupied) {
+            fill('red');
+            let midp = this.midpoint();
+            circle(midp.x, midp.y, 30);
+        }
+        pop();
+    }
+
+    update() {
+
+
+    }
+
+}
